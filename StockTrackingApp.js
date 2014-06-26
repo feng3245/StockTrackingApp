ccd54f43dacbe4b5ed7834ecccf383c537da20fb
feng3245 <li123b@hotmail.com>
2014-06-25 23:05:24 -0400

    reverted back to email implementation

--- StockTrackingApp.js
+++ StockTrackingApp.js
@@ -14,22 +14,43 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-   
-  Meteor.startup(function () {
-     
-    fs = Npm.require('fs');
-    path = "./";
-    fs.writeFile(name = (path + "whatever"), scrappedData.findOne({category:"content"}).content, encoding = "utf8", function(err) {
-    if (err) {
-      throw (new Meteor.Error(500, 'Failed to save file.', err));
-    } else {
-      console.log('The file ' + name + ' (' + encoding + ') was saved to ' + path);
-    }
-  });
-    //saveFile(scrappedData.findOne({category:"content"}).content, "whatever", "./", "ANSI");
-    scrape("http://ca.indeed.com/jobs?q=hr+assistant&l=Toronto,+ON&rq=1&fromage=last");
-    
-    console.log(indeedParse(scrappedData.findOne({category:"content"}).content));
 
+  Meteor.startup(function () {
+    scrape("http://ca.indeed.com/jobs?q=hr+assistant&l=Toronto,+ON&rq=1&fromage=last");
+    Meteor.setInterval(
+   function()
+   {
+    writeToMail = function(content)
+  {
+        fs = Npm.require('fs');
+        path = ".\\..\\..\\..\\..\\inetpub\\mailroot\\Pickup";
+    //scrappedData.findOne({category:"content"}).content
+    fs.writeFile(name = (path + "whatever"), content, encoding = "utf8", function(err) 
+    {
+      if (err) 
+      {
+        throw (new Meteor.Error(500, 'Failed to save file.', err));
+      } 
+      else 
+      {
+        console.log('The file ' + name + ' (' + encoding + ') was saved to ' + path);
+      }
+    });
+  }
+        if(scrappedData.findOne({category:"content"}).content!=undefined)
+        writeToMail(scrappedData.findOne({category:"content"}).content);
+  
+
+}
+      
+
+  
+      
+      
+,30000);
+    
   });
 }
+    
+    
+
